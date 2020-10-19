6c4c640cc5d2af8c20a908f7282f3d3c2e67f072
arthurparahyba <arthur.parahyba@gmail.com>
2020-10-19 19:40:12 -0300

    ajustes

--- src/test/java/br/com/parahyba/conversor/ConversorApplicationTests.java
+++ src/test/java/br/com/parahyba/conversor/ConversorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package br.com.parahyba.conversor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ConversorApplicationTests {
 
 	@Test
 	void contextLoads() {
